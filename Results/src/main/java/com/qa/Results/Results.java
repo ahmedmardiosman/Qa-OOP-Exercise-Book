7baf3cf128694d4c0c2a15be2966c4172cd2eba0
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-15 02:24:56 +0100

    First Commit

---
+++ Results/src/main/java/com/qa/Results/Results.java
@@ -0,0 +1,68 @@
+package com.qa.Results;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Results {
+
+	int physics;
+	int chemistry;
+	int biology;
+	int total;
+	double percentage;
+
+	public Results(int physics, int chemistry, int biology) {
+		this.physics = physics;
+		this.chemistry = chemistry;
+		this.biology = biology;
+
+	}
+
+	public List<Integer> listOfMarks() {
+
+		List<Integer> resultsList = new ArrayList<Integer>();
+
+		total = physics + chemistry + biology;
+
+		resultsList.add(physics);
+		resultsList.add(chemistry);
+		resultsList.add(biology);
+		resultsList.add(total);
+
+		return resultsList;
+
+	}
+
+	public double percentage() {
+
+		double total = physics + chemistry + biology;
+
+		percentage = (total * 100) / 450;
+
+		return percentage;
+	}
+
+	public void passOrFail() {
+
+		if (percentage() < 60 || physics < 90 || chemistry < 90 || biology < 90) {
+			System.out.println("Unfortunatley, you have failed");
+			
+			if(physics<90) {
+			System.out.println("You have not met the mimimum threshold to pass Physics");
+			}
+			
+			if(chemistry<90) {
+				System.out.println("You have not met the mimimum threshold to pass Chemistry");
+				}
+			
+			if(biology<90) {
+				System.out.println("You have not met the mimimum threshold to pass Biology");
+				}
+			
+
+		} else {
+			System.out.println("Congratulations, you have passed \nYou have met the minimum grades for each subject");
+		}
+	}
+
+}
---
+++ Results/src/main/java/com/qa/Results/Runner.java
@@ -0,0 +1,25 @@
+package com.qa.Results;
+
+public class Runner {
+
+	public static void main(String[] args) {
+			
+
+		Results studentResults = new Results(10, 20, 342);
+
+//		System.out.println(
+//				"The following list shows results for physics, biology, chemistry and the total result of all three exams:");
+//		studentResults.listOfMarks().forEach(System.out::println);
+//		
+		System.out.println("The students Physics mark was: " + studentResults.listOfMarks().get(0));
+		System.out.println("The students Chemistry mark was: " + studentResults.listOfMarks().get(1));
+		System.out.println("The students Biology mark was: " + studentResults.listOfMarks().get(2));
+		System.out.println("The students total marks: " + studentResults.listOfMarks().get(3));
+		
+		System.out.println("The students overall percentage for all exams taken was : " + studentResults.percentage());
+		
+		studentResults.passOrFail();
+	
+	}
+
+}
